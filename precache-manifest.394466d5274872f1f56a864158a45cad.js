5f348a55f5d268ce0ff683859110cdc8244f1db4
rfitkin90 <rfitkin90@gmail.com>
2019-03-26 08:02:32 -0700

    Updates

---
+++ precache-manifest.394466d5274872f1f56a864158a45cad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "45e275b16884f68fb9ed9077709dbf87",
+    "url": "/my-app/static/media/kitten-banner.45e275b1.jpg"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "f574a015903a254e0439",
+    "url": "/my-app/static/js/main.be242297.chunk.js"
+  },
+  {
+    "revision": "4a2fb9f95ad59b49fc9e",
+    "url": "/my-app/static/js/2.12616e00.chunk.js"
+  },
+  {
+    "revision": "f574a015903a254e0439",
+    "url": "/my-app/static/css/main.8f559166.chunk.css"
+  },
+  {
+    "revision": "4a2fb9f95ad59b49fc9e",
+    "url": "/my-app/static/css/2.5fdf4a27.chunk.css"
+  },
+  {
+    "revision": "cb42588b57f8861196e029b89752fc52",
+    "url": "/my-app/index.html"
+  }
+];
